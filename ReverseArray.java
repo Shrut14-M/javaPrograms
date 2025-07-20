a0c2a3e10d092c674f3a81a041215aeec5401c5f
Shruti Manoorkar <shrutimanoorkar1404@gmail.com>
2025-07-20 23:41:22 +0530

    Add files via upload
    Java Program to Print Array in Reverse Order -
    
    This program demonstrates how to print the elements of an array in reverse order. It initializes an integer array with predefined values and uses a for loop to traverse the array from the last index to the first. Each element is printed as the loop moves backward through the array.

---
+++ ReverseArray.java
@@ -0,0 +1,11 @@
+package ArrayPractice;
+
+public class ReverseArray {
+public static void main(String[] args) {
+	int arr[]= {6,3,7,2,8};
+	for(int i=arr.length-1;i>=0;i--) {
+		System.out.println(arr[i]);
+		
+	}
+}
+}
