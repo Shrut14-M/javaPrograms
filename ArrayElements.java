efe8aa3ae9cf71249fa3eef73742151b005e912b
Shruti Manoorkar <shrutimanoorkar1404@gmail.com>
2025-07-20 23:37:48 +0530

    Add files via upload
    ⭐ Printing Array Elements in Java :
    
    📌 The program initializes an array of integers with predefined values.
    🔁 It uses a for loop to traverse through the array using index positions.
    🖨️ Each element of the array is printed on the console, one by one.
    🧠 This demonstrates basic array traversal and output in Java.

---
+++ ArrayElements.java
@@ -0,0 +1,14 @@
+package ArrayPractice;
+
+import java.util.Arrays;
+
+public class ArrayElements {
+public static void main(String[] args) {
+	int arr[]= {7,4,8,9,2,10};
+	System.out.println("Elements of array are");
+
+	for(int i=0;i<arr.length;i++) {
+		System.out.println(arr[i]);
+		}
+}
+}
